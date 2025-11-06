e5ba40f2e2a8fd53d71835435a2f83f103baca24
chakribaggam456 <chakrachandb@gmail.com>
2025-11-06 13:44:09 +0400

    second

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_support   = false
+  enable_dns_hostnames = false
   tags = {
     Name = var.vpc_name
   }
